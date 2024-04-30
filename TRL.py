a0ea0a920ae205a6ff2c77aeda2924cd5d751aaf
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-04-30 19:04:21 -0400

    Update TRL.py

--- TRL.py
+++ TRL.py
@@ -1,75 +1,61 @@
+import wx
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from mpl_toolkits.mplot3d import Axes3D
 
 class RingLaserExperiment:
-    def __init__(self, laser_power, laser_radius, num_detectors, num_points=1000, c=299792458, G=6.67430e-11):
+    def __init__(self, laser_power, laser_radius):
         self.laser_power = laser_power
         self.laser_radius = laser_radius
-        self.num_detectors = num_detectors
-        self.num_points = num_points
-        self.c = c
-        self.G = G
+        self.c = 299792458
+        self.G = 6.67430e-11
 
-    def calculate_frame_dragging_effect(self, obj):
-        angular_momentum = np.linalg.norm(np.cross(obj.position, obj.velocity))
-        frame_dragging_effect = (2 * self.G * angular_momentum) / (self.c ** 2 * np.linalg.norm(obj.position) ** 3)
-        return frame_dragging_effect
+    def simulate_experiment(self):
+        theta = np.linspace(0, 2 * np.pi, 360)
+        x = self.laser_radius * np.cos(theta)
+        y = self.laser_radius * np.sin(theta)
+        z = np.zeros(360)
+        gravitational_effect = self.G * self.laser_power / (self.laser_radius * self.c ** 2)
+        time_dilation = np.sqrt(1 - 2 * gravitational_effect / (self.c ** 2))
+        return x, y, z, time_dilation
 
-    def calculate_time_dilation(self, obj):
-        gravitational_potential = -self.G * obj.mass / np.linalg.norm(obj.position)
-        time_dilation_factor = np.sqrt(1 - 2 * gravitational_potential / (self.c ** 2))
-        return time_dilation_factor
+class MainFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title="Ring Laser Experiment", size=(800, 600))
+        self.experiment = RingLaserExperiment(laser_power=1e-3, laser_radius=1e9)
+        
+        self.panel = wx.Panel(self)
+        self.fig, self.ax = plt.subplots(subplot_kw={'projection': '3d'})
+        self.canvas = FigureCanvas(self.panel, -1, self.fig)
+        
+        self.power_slider = wx.Slider(self.panel, value=1, minValue=1, maxValue=10, pos=(20, 20), size=(250, -1))
+        self.radius_slider = wx.Slider(self.panel, value=1000, minValue=100, maxValue=5000, pos=(20, 70), size=(250, -1))
+        
+        self.power_slider.Bind(wx.EVT_SCROLL, self.update_experiment)
+        self.radius_slider.Bind(wx.EVT_SCROLL, self.update_experiment)
+        
+        self.update_plot()
 
-    def generate_points(self, obj):
-        points = np.random.uniform(low=obj.min_bound, high=obj.max_bound, size=(self.num_points, 3))
-        return points
+    def update_experiment(self, event):
+        power = self.power_slider.GetValue() * 1e-4
+        radius = self.radius_slider.GetValue() * 1e7
+        self.experiment.laser_power = power
+        self.experiment.laser_radius = radius
+        self.update_plot()
 
-    def simulate_experiment(self, scene):
-        frame_dragging_effects = np.zeros(len(scene.objects))
-        time_dilation_factors = np.zeros(len(scene.objects))
-        for i, obj in enumerate(scene.objects):
-            frame_dragging_effects[i] = self.calculate_frame_dragging_effect(obj)
-            time_dilation_factors[i] = self.calculate_time_dilation(obj)
-        return frame_dragging_effects, time_dilation_factors
+    def update_plot(self):
+        x, y, z, time_dilation = self.experiment.simulate_experiment()
+        self.ax.clear()
+        sc = self.ax.scatter(x, y, z, c=time_dilation, cmap='viridis')
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.set_zlabel('Z')
+        self.fig.colorbar(sc, ax=self.ax, label='Time Dilation Factor')
+        self.canvas.draw()
 
-    def visualize_spacetime_distortion(self, point_cloud, frame_dragging_effects, time_dilation_factors):
-        fig = plt.figure(figsize=(10, 8))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(point_cloud[:, 0], point_cloud[:, 1], point_cloud[:, 2], c=frame_dragging_effects, cmap='viridis', marker='.')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        plt.title("Spacetime Distortion induced by Ring Laser Experiment")
-        plt.colorbar(label='Frame Dragging Effect')
-        plt.show()
-
-class MassiveObject:
-    def __init__(self, mass, position, velocity):
-        self.mass = mass
-        self.position = position
-        self.velocity = velocity
-        self.min_bound = np.array([-1e9, -1e9, -1e9])
-        self.max_bound = np.array([1e9, 1e9, 1e9])
-
-class Scene:
-    def __init__(self, objects):
-        self.objects = objects
-
-black_hole1 = MassiveObject(mass=10 ** 36, position=np.array([0, 0, 0]), velocity=0)
-black_hole2 = MassiveObject(mass=5 * 10 ** 35, position=np.array([1e8, 0, 0]), velocity=1e7)
-black_hole3 = MassiveObject(mass=2 * 10 ** 35, position=np.array([-1e8, 0, 0]), velocity=-1e7)
-scene = Scene(objects=[black_hole1, black_hole2, black_hole3])
-
-ring_laser_experiment = RingLaserExperiment(
-    laser_power=1e-3,
-    laser_radius=1e9,
-    num_detectors=360,
-    num_points=5000,
-    c=299792458,
-    G=6.67430e-11
-)
-
-frame_dragging_effects, time_dilation_factors = ring_laser_experiment.simulate_experiment(scene)
-point_cloud = ring_laser_experiment.generate_points(black_hole1)
-ring_laser_experiment.visualize_spacetime_distortion(point_cloud, frame_dragging_effects, time_dilation_factors)
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = MainFrame()
+    frame.Show()
+    app.MainLoop()
